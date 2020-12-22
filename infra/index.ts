d97457c9a8312252c7303cf58a87c0917e89326b
Lcmkey <lcmkey@gmail.com>
2020-12-23 06:27:24 +0800

    update

--- infra/index.ts
+++ infra/index.ts
@@ -1,11 +1,11 @@
-import * as cdk from '@aws-cdk/core'
+import * as cdk from "@aws-cdk/core"
 
-import { PipelineStack } from './stacks/pipeline'
+import { PipelineStack } from "./stacks/pipeline"
 
 const app = new cdk.App()
 
-new PipelineStack(app, 'PipelineStack', {
-    env: { region: 'us-east-1' },
+new PipelineStack(app, "PipelineStack", {
+    env: { region: "us-east-1" },
 });
 
-app.synth()
+app.synth();
--- infra/stacks/pipeline.ts
+++ infra/stacks/pipeline.ts
@@ -1,8 +1,8 @@
-import { Stack, App, StackProps, SecretValue, RemovalPolicy, CfnOutput } from '@aws-cdk/core'
-import { PipelineProject, BuildSpec } from '@aws-cdk/aws-codebuild'
-import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3'
-import { Pipeline, Artifact } from '@aws-cdk/aws-codepipeline'
-import { GitHubSourceAction, GitHubTrigger, CodeBuildAction, S3DeployAction, ManualApprovalAction } from '@aws-cdk/aws-codepipeline-actions'
+import { Stack, App, StackProps, SecretValue, RemovalPolicy, CfnOutput } from "@aws-cdk/core"
+import { PipelineProject, BuildSpec } from "@aws-cdk/aws-codebuild"
+import { Bucket, BucketEncryption } from "@aws-cdk/aws-s3"
+import { Pipeline, Artifact } from "@aws-cdk/aws-codepipeline"
+import { GitHubSourceAction, GitHubTrigger, CodeBuildAction, S3DeployAction, ManualApprovalAction } from "@aws-cdk/aws-codepipeline-actions"
 
 export interface PipelineProps extends StackProps {
 }
@@ -14,14 +14,14 @@ export class PipelineStack extends Stack {
         /**
          * Amazon S3 bucket to store CRA website
          */
-        const pipelineArtifactBucket = new Bucket(this, 'CiCdPipelineArtifacts', {
-            bucketName: `ci-cd-pipeline-artifacts-`,
+        const pipelineArtifactBucket = new Bucket(this, "CRAPipelineArtifact", {
+            bucketName: `cra-pipeline-artifact`,
             encryption: BucketEncryption.S3_MANAGED,
             removalPolicy: RemovalPolicy.DESTROY, // NOT recommended for production code
         });
-        const bucketWebsite = new Bucket(this, 'Files', {
-            websiteIndexDocument: 'index.html',
-            websiteErrorDocument: 'error.html',
+        const bucketWebsite = new Bucket(this, "Files", {
+            websiteIndexDocument: "index.html",
+            websiteErrorDocument: "error.html",
             publicReadAccess: true,
             removalPolicy: RemovalPolicy.DESTROY, // NOT recommended for production code
         })
@@ -36,22 +36,22 @@ export class PipelineStack extends Stack {
          * Source - Github
          */
         const sourceAction: GitHubSourceAction = new GitHubSourceAction({
-            actionName: 'Source',
+            actionName: "Source",
             owner: "Lcmkey",
-            repo: 'aws-cdk-cra-piepline-deploy',
-            oauthToken: SecretValue.secretsManager('GitHubToken'),
+            repo: "aws-cdk-cra-piepline-deploy",
+            oauthToken: SecretValue.secretsManager("GitHubToken"),
             output: sourceArtifact,
-            branch: 'master',
+            branch: "master",
             trigger: GitHubTrigger.POLL,
-            variablesNamespace: 'SourceVariables'
+            variablesNamespace: "SourceVariables"
         });
 
         /**
          * Pipeline Project - Build
          */
-        const buildProject: PipelineProject = new PipelineProject(this, 'BuildWebsite', {
-            projectName: 'website-source-build',
-            buildSpec: BuildSpec.fromSourceFilename('./infra/buildspec.yml'),
+        const buildProject: PipelineProject = new PipelineProject(this, "BuildWebsite", {
+            projectName: "website-source-build",
+            buildSpec: BuildSpec.fromSourceFilename("./infra/buildspec.yml"),
             // environment: {
             //     buildImage: LinuxBuildImage.STANDARD_3_0
             // },
@@ -61,7 +61,7 @@ export class PipelineStack extends Stack {
          * Code Build Action - Build Source
          */
         const buildAction: CodeBuildAction = new CodeBuildAction({
-            actionName: 'Build',
+            actionName: "Build",
             project: buildProject,
             input: sourceArtifact,
             outputs: [buildArtifact],
@@ -71,7 +71,7 @@ export class PipelineStack extends Stack {
          * AWS CodePipeline action to deploy CRA website to S3
          */
         const DeployAction = new S3DeployAction({
-            actionName: 'Deploy',
+            actionName: "Deploy",
             input: buildArtifact,
             bucket: bucketWebsite,
             runOrder: 2
@@ -81,16 +81,16 @@ export class PipelineStack extends Stack {
          * Manual Approval Action - Production
          */
         const manualApprovalAction: ManualApprovalAction = new ManualApprovalAction({
-            actionName: 'Review',
-            additionalInformation: 'Agree to deploy',
+            actionName: "Review",
+            additionalInformation: "Agree to deploy",
             runOrder: 1
         });
 
         /**
          * Create Pipeline
          */
-        new Pipeline(this, 'CiCdPipeline', {
-            pipelineName: 'CRA-Website',
+        new Pipeline(this, "CiCdPipeline", {
+            pipelineName: "CRA-Website",
             artifactBucket: pipelineArtifactBucket,
             restartExecutionOnUpdate: true,
             stages: [
@@ -98,27 +98,27 @@ export class PipelineStack extends Stack {
                     /**
                      * AWS CodePipeline stage to clone sources from GitHub repository
                      */
-                    stageName: 'Source',
+                    stageName: "Source",
                     actions: [sourceAction],
                 }, {
                     /**
                      * AWS CodePipeline stage to build CRA website and CDK resources
                      */
-                    stageName: 'Build',
+                    stageName: "Build",
                     actions: [buildAction]
                 }, {
                     /**
                      * AWS CodePipeline stage to deployt CRA website and CDK resources
                      */
-                    stageName: 'Deploy',
+                    stageName: "Deploy",
                     actions: [manualApprovalAction, DeployAction]
                 }
             ]
         });
 
-        new CfnOutput(this, 'WebsiteURL', {
+        new CfnOutput(this, "WebsiteURL", {
             value: bucketWebsite.bucketWebsiteUrl,
-            description: 'Website URL',
+            description: "Website URL",
         })
     }
 }
